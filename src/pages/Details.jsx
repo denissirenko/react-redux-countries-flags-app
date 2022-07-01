c47e7ee209ddfaba879b8c5fea0e3d2de57aa99f
Denis Sirenko <sirenko.denis86@gmail.com>
2022-07-01 11:33:51 +0300

    add details coutry page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,18 +1,31 @@
+import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { IoArrowBack } from 'react-icons/io5';
 
 import { Button } from '../components/Button';
 import { Info } from '../components/Info';
-
+import { selectDetails } from '../store/details/details-selectors';
+import { clearDetails, loadCountryByName } from '../store/details/details-actions';
 
 export const Details = () => {
+  const dispatch = useDispatch();
   const { name } = useParams();
   const navigate = useNavigate();
 
-  const currentCountry = null;
+  const { currentCountry, error, status } = useSelector(selectDetails);
+
+  useEffect(() => {
+    dispatch(loadCountryByName(name));
+    return () => {
+      dispatch(clearDetails());
+    };
+  }, [name, dispatch]);
 
   return (
     <div>
+      {error && <h2>Can't fetch data</h2>}
+      {status === 'loading' && <h2>Loading...</h2>}
       <Button onClick={() => navigate(-1)}>
         <IoArrowBack /> Back
       </Button>
---
+++ src/store/details/details-reducer.js
@@ -0,0 +1,34 @@
+import { SET_COUNTRY, SET_ERROR, SET_LOADING, CLEAR_DETAILS } from './details-actions';
+
+const initialState = {
+  currentCountry: null,
+  status: 'idle',
+  error: null,
+};
+
+export const detailsReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case SET_LOADING:
+      return {
+        ...state,
+        error: null,
+        status: 'loading',
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        status: 'rejected',
+        error: payload,
+      };
+    case SET_COUNTRY:
+      return {
+        ...state,
+        status: 'received',
+        currentCountry: payload,
+      };
+    case CLEAR_DETAILS:
+      return initialState;
+    default:
+      return state;
+  }
+};
